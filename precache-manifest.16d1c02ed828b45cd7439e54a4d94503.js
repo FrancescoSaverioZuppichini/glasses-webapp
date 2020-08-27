73ea8f4db3e7636c7183f88c339ab6c0bac58f24
FrancescoSaverioZuppichini <zuppif@usi.ch>
2020-08-27 12:09:56 +0000

    Deploying to gh-pages from  @ e3e782a62d39440f98633594899632cc09eeed19 🚀

---
+++ precache-manifest.16d1c02ed828b45cd7439e54a4d94503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c92635608ffa9275283934125364fc",
+    "url": "/glasses-webapp/index.html"
+  },
+  {
+    "revision": "cbc05ca16ae8ed5c6ac3",
+    "url": "/glasses-webapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/glasses-webapp/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/glasses-webapp/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc05ca16ae8ed5c6ac3",
+    "url": "/glasses-webapp/static/js/main.9d4fb465.chunk.js"
+  },
+  {
+    "revision": "37dcad765b6c16529aa8",
+    "url": "/glasses-webapp/static/js/runtime-main.82082ff0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/glasses-webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31183b2eb7b4a461113496dfbbde51a0.js"
+  "/glasses-webapp/precache-manifest.16d1c02ed828b45cd7439e54a4d94503.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/glasses-webapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
